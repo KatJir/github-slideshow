abb10204afeb532e47e5f6d9c410f72f4dfa9522
KatJir <81764562+KatJir@users.noreply.github.com>
2021-04-12 20:19:58 +0200

    Update homework.jl

--- homework.jl
+++ homework.jl
@@ -4,9 +4,9 @@ Pkg.activate("./homework_01")
 Pkg.instantiate()
 include("newton.jl")
 
-#f(x) = [exp(x[1]^2 + x[2]^2 - 1) + (x[1] - 1)^2]
-#g(x) = [2*x[1]*(exp(x[1]^2 + x[2]^2 - 1)) + 2*(x[1] - 1); 2*x[2]*(exp(x[1]^2 + x[2]^2 - 1))]
-#x_root = newton(f, g, [0.0; 0.0])
+f(x) = [exp(x[1]^2 + x[2]^2 - 1) + (x[1] - 1)^2]
+g(x) = [2*x[1]*(exp(x[1]^2 + x[2]^2 - 1)) + 2*(x[1] - 1); 2*x[2]*(exp(x[1]^2 + x[2]^2 - 1))]
+x_root = newton(f, g, [0.0; 0.0])
 
 
 
